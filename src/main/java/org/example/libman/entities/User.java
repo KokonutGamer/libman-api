8566be0351f7b9bc0b82c7ddf9f6d0fb2c3bb507
Gabe Lapingcao <mgultra2018@gmail.com>
2024-12-29 19:24:20 -0800

    Implemented UserDetails within the User entity

--- src/main/java/org/example/libman/entities/User.java
+++ src/main/java/org/example/libman/entities/User.java
@@ -4,15 +4,19 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 @Table(name = "users")
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false)
@@ -35,4 +39,29 @@ public class User {
     @UpdateTimestamp
     @Column(name = "updated_at")
     private Date updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true; // TODO determine logic for these methods
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true; // TODO determine logic for these methods
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true; // TODO determine logic for these methods
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true; // TODO determine logic for these methods
+    }
 }
