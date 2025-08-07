68e27d618474021263a4c72ed4b37e90ed6f195b
Gabe Lapingcao <mgultra2018@gmail.com>
2025-08-06 21:11:11 -0700

    Implemented Genre entity

---
+++ src/main/java/org/example/libman/entities/Genre.java
@@ -0,0 +1,30 @@
+package org.example.libman.entities;
+
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "genre")
+public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(length = 30)
+    private String name;
+
+    @ManyToMany(mappedBy = "genres")
+    private Set<Book> books;
+}
