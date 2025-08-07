3145e05acea7cb93b2a7b525e984900e6083b9c8
Gabe Lapingcao <mgultra2018@gmail.com>
2025-08-06 21:11:00 -0700

    Implemented Author entity

---
+++ src/main/java/org/example/libman/entities/Author.java
@@ -0,0 +1,33 @@
+package org.example.libman.entities;
+
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "author")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "first_name", length = 30)
+    private String firstName;
+
+    @Column(name = "last_name", length = 30)
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> writtenBooks;
+}
