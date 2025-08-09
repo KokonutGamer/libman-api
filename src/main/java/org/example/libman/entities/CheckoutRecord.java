eaa5b8d6b5cb8ef4c340953254ab782ad75169f3
Gabe Lapingcao <mgultra2018@gmail.com>
2025-08-09 00:58:33 -0700

    Implemented CheckoutRecord entity

---
+++ src/main/java/org/example/libman/entities/CheckoutRecord.java
@@ -0,0 +1,42 @@
+package org.example.libman.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "checkout_record")
+public class CheckoutRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private LocalDateTime date;
+
+    @Column(name = "check-in_due")
+    private LocalDateTime checkInDue;
+
+    @Column(name = "check-in_date", nullable = true)
+    private LocalDateTime checkInDate;
+
+    // Relationships
+    @ManyToOne
+    @JoinColumn(name = "library_card_barcode")
+    private LibraryCard checkoutCard;
+
+    @ManyToOne
+    @JoinColumn(name = "book_copy_barcode")
+    private BookCopy checkedOutBook;
+}
