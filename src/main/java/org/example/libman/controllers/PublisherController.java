da0a40ab4be00e907e0ae2d50c7c51901e1b4638
Gabe Lapingcao <mgultra2018@gmail.com>
2025-08-12 20:40:19 -0700

    Implemented Publisher assembler, controller, exception, and repository (somehow in another commit)

---
+++ src/main/java/org/example/libman/controllers/PublisherController.java
@@ -0,0 +1,42 @@
+package org.example.libman.controllers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.example.libman.assemblers.PublisherModelAssembler;
+import org.example.libman.entities.Publisher;
+import org.example.libman.exceptions.PublisherNotFoundException;
+import org.example.libman.repositories.PublisherRepository;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PublisherController {
+
+    private final PublisherRepository publisherRepository;
+    private final PublisherModelAssembler publisherAssembler;
+
+    public PublisherController(PublisherRepository publisherRepository, PublisherModelAssembler publisherAssembler) {
+        this.publisherRepository = publisherRepository;
+        this.publisherAssembler = publisherAssembler;
+    }
+
+    @GetMapping("/publishers")
+    public CollectionModel<EntityModel<Publisher>> all() {
+        List<EntityModel<Publisher>> publishers = publisherRepository.findAll().stream()
+                .map(publisherAssembler::toModel)
+                .collect(Collectors.toList());
+        return CollectionModel.of(publishers, linkTo(methodOn(PublisherController.class).all()).withSelfRel());
+    }
+
+    @GetMapping("/publishers/{id}")
+    public EntityModel<Publisher> one(@PathVariable Integer id) {
+        Publisher publisher = publisherRepository.findById(id).orElseThrow(() -> new PublisherNotFoundException(id));
+        return publisherAssembler.toModel(publisher);
+    }
+}
