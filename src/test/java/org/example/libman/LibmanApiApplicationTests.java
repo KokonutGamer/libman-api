743e08e58dc85c19f220e6762ffdb5b3e9b94ccf
Gabe Lapingcao <mgultra2018@gmail.com>
2025-08-12 21:44:26 -0700

    Removed artifacts from previous version

--- src/test/java/org/example/libman/LibmanApiApplicationTests.java
+++ src/test/java/org/example/libman/LibmanApiApplicationTests.java
@@ -1,24 +1,7 @@
 package org.example.libman;
 
-import org.example.libman.controllers.BookController;
-import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class LibmanApiApplicationTests {
-
-	private static final Logger log = LoggerFactory.getLogger(LibmanApiApplicationTests.class);
-
-	@Autowired
-	private BookController bookController;
-
-	@Test
-	void checkBookControllerOnLoad() {
-		assertThat(bookController).isNotNull();
-	}
 }
