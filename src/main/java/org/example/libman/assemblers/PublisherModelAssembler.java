280a27528814121776b1b954e98ae0bb24028c0f
Gabe Lapingcao <mgultra2018@gmail.com>
2025-08-15 17:52:50 -0700

    Refactored PublisherModelAssembler to abide by HAL conventions

--- src/main/java/org/example/libman/assemblers/PublisherModelAssembler.java
+++ src/main/java/org/example/libman/assemblers/PublisherModelAssembler.java
@@ -1,8 +1,15 @@
 package org.example.libman.assemblers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.example.libman.controllers.BookController;
 import org.example.libman.controllers.PublisherController;
+import org.example.libman.dtos.BookModel;
+import org.example.libman.dtos.PublisherModel;
 import org.example.libman.entities.Publisher;
-import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.mediatype.hal.HalModelBuilder;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -10,11 +17,23 @@ import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PublisherModelAssembler implements RepresentationModelAssembler<Publisher, EntityModel<Publisher>> {
+public class PublisherModelAssembler implements RepresentationModelAssembler<Publisher, RepresentationModel<?>> {
     @Override
-    public @NonNull EntityModel<Publisher> toModel(@NonNull Publisher publisher) {
-        return EntityModel.of(publisher,
-                linkTo(methodOn(PublisherController.class).one(publisher.getId())).withSelfRel(),
-                linkTo(methodOn(PublisherController.class).all()).withRel("publishers"));
+    public @NonNull RepresentationModel<?> toModel(@NonNull Publisher publisher) {
+        PublisherModel model = PublisherModel.fromEntity(publisher);
+
+        List<BookModel> booksEmbedded = publisher.getPublishedBooks().stream().map(b -> {
+            BookModel bookModel = BookModel.fromEntity(b);
+            bookModel.add(linkTo(methodOn(BookController.class).one(b.getIsbn())).withSelfRel());
+            return bookModel;
+        }).collect(Collectors.toList());
+
+        RepresentationModel<?> halModel = HalModelBuilder.halModelOf(model)
+                .embed(booksEmbedded)
+                .link(linkTo(methodOn(PublisherController.class).one(publisher.getId())).withSelfRel())
+                .link(linkTo(methodOn(PublisherController.class).all()).withRel("publishers"))
+                .build();
+
+        return halModel;
     }
 }
--- src/main/java/org/example/libman/controllers/PublisherController.java
+++ src/main/java/org/example/libman/controllers/PublisherController.java
@@ -8,7 +8,7 @@ import org.example.libman.entities.Publisher;
 import org.example.libman.exceptions.PublisherNotFoundException;
 import org.example.libman.repositories.PublisherRepository;
 import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.RepresentationModel;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,15 +27,15 @@ public class PublisherController {
     }
 
     @GetMapping("/publishers")
-    public CollectionModel<EntityModel<Publisher>> all() {
-        List<EntityModel<Publisher>> publishers = publisherRepository.findAll().stream()
+    public CollectionModel<RepresentationModel<?>> all() {
+        List<RepresentationModel<?>> publishers = publisherRepository.findAll().stream()
                 .map(publisherAssembler::toModel)
                 .collect(Collectors.toList());
         return CollectionModel.of(publishers, linkTo(methodOn(PublisherController.class).all()).withSelfRel());
     }
 
     @GetMapping("/publishers/{id}")
-    public EntityModel<Publisher> one(@PathVariable Integer id) {
+    public RepresentationModel<?> one(@PathVariable Integer id) {
         Publisher publisher = publisherRepository.findById(id).orElseThrow(() -> new PublisherNotFoundException(id));
         return publisherAssembler.toModel(publisher);
     }
