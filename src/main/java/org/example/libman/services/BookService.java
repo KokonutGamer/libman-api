69550d41d3584a0fc26b4c7bd5ef6f373c9e0390
Gabe Lapingcao <mgultra2018@gmail.com>
2025-08-21 20:19:19 -0700

    Refactored saveBook method to check whether Book, Publisher, Author(s), or Genre(s) already exist

--- src/main/java/org/example/libman/services/BookService.java
+++ src/main/java/org/example/libman/services/BookService.java
@@ -14,6 +14,7 @@ import org.example.libman.repositories.AuthorRepository;
 import org.example.libman.repositories.BookRepository;
 import org.example.libman.repositories.GenreRepository;
 import org.example.libman.repositories.PublisherRepository;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -33,30 +34,55 @@ public class BookService {
     }
 
     public Book saveBook(BookDTO bookDTO) {
-        // TODO refactor to check if the given book already exists
+        if (bookRepository.existsById(bookDTO.getIsbn())) {
+            return bookRepository.getReferenceById(bookDTO.getIsbn());
+        }
 
         Book book = Book.fromDTO(bookDTO);
 
-        // TODO refactor to check if the given publisher already exists
-        Publisher publisher = new Publisher();
-        publisher.setName(bookDTO.getPublisherName());
-        publisher = publisherRepository.save(publisher);
-        book.setPublisher(publisher);
+        Publisher publisherProbe = new Publisher();
+        publisherProbe.setName(bookDTO.getPublisherName());
 
-        // TODO refactor to check if the given author already exists
+        publisherRepository.findOne(Example.of(publisherProbe)).ifPresentOrElse(p -> {
+            p.getPublishedBooks().add(book);
+            book.setPublisher(p);
+        }, () -> {
+            Publisher publisher = new Publisher();
+            publisher.setName(bookDTO.getPublisherName());
+            publisher = publisherRepository.save(publisher);
+            publisher.setPublishedBooks(new HashSet<>());
+            publisher.getPublishedBooks().add(book);
+            book.setPublisher(publisher);
+        });
+
         book.setAuthors(new HashSet<>());
         for (AuthorDTO authorDTO : bookDTO.getAuthors()) {
-            Author author = authorRepository.save(Author.fromDTO(authorDTO));
-            book.getAuthors().add(author);
+            Author authorProbe = Author.fromDTO(authorDTO);
+            authorRepository.findOne(Example.of(authorProbe)).ifPresentOrElse(a -> {
+                a.getWrittenBooks().add(book);
+                book.getAuthors().add(a);
+            }, () -> {
+                Author author = authorRepository.save(Author.fromDTO(authorDTO));
+                book.getAuthors().add(author);
+            });
+
         }
 
-        // TODO refactor to check if the given genre already exists
         book.setGenres(new HashSet<>());
         for (String genreName : bookDTO.getGenres()) {
-            Genre genre = new Genre();
-            genre.setName(genreName);
-            genre = genreRepository.save(genre);
-            book.getGenres().add(genre);
+            Genre genreProbe = new Genre();
+            genreProbe.setName(genreName);
+
+            genreRepository.findOne(Example.of(genreProbe)).ifPresentOrElse(g -> {
+                g.getBooksWithinGenre().add(book);
+                book.getGenres().add(g);
+            }, () -> {
+                Genre genre = new Genre();
+                genre.setName(genreName);
+                genre = genreRepository.save(genre);
+                book.getGenres().add(genre);
+            });
+
         }
 
         bookRepository.save(book);
